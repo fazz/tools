b299a360ed5d89826399ef2306afb30faddbc1f0
Mart Oruaas <fazz@pobox.com>
2021-04-16 14:14:04 +0000

    Updates

---
+++ scripts/ubuntuserver.bash
@@ -0,0 +1,13 @@
+
+apt-get install gcc g++ cmake
+
+apt-get install tasksel
+
+tasksel install lubuntu-desktop
+
+systemctl set-default graphical.target
+
+apt-get install i3
+
+apt-get install virtualbox-guest-x11
+
